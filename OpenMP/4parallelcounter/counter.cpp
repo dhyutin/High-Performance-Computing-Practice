7fa0a933120fb58bececd922daac979dfb72e022
dhyutin <sreedhyutin@gmail.com>
2025-02-06 21:56:30 -0600

    parallel counter

---
+++ OpenMP/4parallelcounter/counter.cpp
@@ -0,0 +1,63 @@
+// code to parallelize a counter 
+
+#include <iostream>
+#include <omp.h>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+int main(){
+
+    omp_set_num_threads(10);
+
+    long long N = 10000000000;
+    long long c = 0;
+
+
+    // serial
+    auto start_s = chrono::high_resolution_clock::now();
+    for(long long i = 0; i <N; i++){
+        c++;
+    }
+    auto end_s = chrono::high_resolution_clock::now();
+
+    double serial = chrono::duration<double>(end_s - start_s).count();
+
+
+    c = 0;
+
+    //parallel
+    // critical makes sure only one thread runs at a time, there by, reduces any race conditions
+    auto start_p = chrono::high_resolution_clock::now();
+    #pragma omp critical
+    {
+        #pragma omp parallel for reduction(+:c)
+        for(long long i = 0; i <N; i++){
+            c++;
+        }
+    }
+    int threads = 0;
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        threads = omp_get_num_threads();
+    }
+    auto end_p = chrono::high_resolution_clock::now();
+
+    double parallel = chrono::duration<double>(end_p - start_p).count();
+
+
+    // Output results
+    cout << "SERIAL" << endl;
+    cout << "Time Taken: " << serial << " seconds" << endl;
+
+    cout << "PARALLEL" << endl;
+    cout << "Num Threads: " << threads << endl;
+    cout << "Time Taken: " << parallel << " seconds" << endl;
+
+    // delete c;
+
+    return 0;
+}
